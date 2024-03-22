6733825c5fdda8e92b63ec8d95e111962f8c5dfc
DevPronob <pbbormon5@gmail.com>
2024-03-22 10:24:51 -0700

    navbar updated

--- src/Pages/Dashbooard/Dashboard.js
+++ src/Pages/Dashbooard/Dashboard.js
@@ -23,8 +23,8 @@ export default function Dashboard() {
 
 
 
-       
-        <div className='col-span-2 h-full  overflow-hidden relative  block '>
+
+        <div className={`col-span-2 h-full  overflow-hidden relative  block`}>
         <Sidebar toggleSidebar ={toggleSidebar} showSidebar ={showSidebar} setShowSidebar ={setShowSidebar}></Sidebar>
         </div>
         <div className='col-span-8 relative'>
